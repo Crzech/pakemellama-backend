8153f0ffea619c96003a052981d6ca68812ad9dd
luisfer <luisfeer20@gmail.com>
2022-04-25 11:54:04 -0600

    GET

--- models/ProductoModel.js
+++ models/ProductoModel.js
@@ -1,21 +1,30 @@
 const sql = require('../db.js');
 
-const Producto = function(producto) {
+const Producto = function (producto) {
     this.Nombre = producto.nombre;
     this.Descripcion = producto.descripcion;
     this.Tipo = producto.tipo;
     this.Precio = producto.precio;
 }
 
-Producto.createOne = function(nuevoProducto, resultado) {
+Producto.createOne = function (nuevoProducto, resultado) {
     sql.query("INSERT INTO Productos SET ?", nuevoProducto, (err, sqlRes) => {
-       if (err) {
-          console.log(err);
-          resultado(err, null); 
-       }
-       console.log("Producto creado con exito", { id: sqlRes.insertId, ...nuevoProducto })
-       resultado(null, { id: sqlRes.insertId, ...nuevoProducto });
-   });
+        if (err) {
+            console.log(err);
+            resultado(err, null);
+        }
+        console.log("Producto creado con exito", { id: sqlRes.insertId, ...nuevoProducto })
+        resultado(null, { id: sqlRes.insertId, ...nuevoProducto });
+    });
 }
-
+Producto.selectAll = function (resultado) {
+    sql.query("SELECT * FROM Productos", null, (err, res) => {
+        if (err) {
+            console.log(err)
+            resultado(err, null)
+        }
+        console.log("Informacion enviada con exito")
+        resultado(null, res)
+    })
+}
 module.exports = Producto;
