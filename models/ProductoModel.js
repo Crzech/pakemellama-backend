065b3bc3893f15bef23e3d29695fcbd68fc6e46f
luisfer <luisfeer20@gmail.com>
2022-05-05 12:57:36 -0600

    PU

--- models/ProductoModel.js
+++ models/ProductoModel.js
@@ -26,8 +26,8 @@ Producto.selectAll = function (resultado) {
     })
 }
 
-Producto.update = function (req, resultado) {
-    sql.query(`UPDATE Productos SET Nombre = ??, Descripcion = ??, Precio = ??, Tipo = ?? WHERE id = ??`, [req.nombre], [req.descripcion], [req.precio], [req.tipo], [req.id], (err, sqlRes) => {
+Producto.update = function (Producto, resultado) {
+    sql.query(`UPDATE Productos SET Nombre = ??, Descripcion = ??, Precio = ??, Tipo = ?? WHERE id = ??`, [Producto.nombre], [Producto.descripcion], [Producto.precio], [Producto.tipo], [Producto.id], (err, sqlRes) => {
         if (err) {
             console.log(err);
             resultado(err, null);
