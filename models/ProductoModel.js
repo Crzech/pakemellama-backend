df23e3b0c2387449bef07fb4af9f0048708f558e
luisfer <luisfeer20@gmail.com>
2022-05-11 10:26:30 -0600

    PUT

--- models/ProductoModel.js
+++ models/ProductoModel.js
@@ -34,14 +34,14 @@ const values = [
     Producto.id,
 ]
 const updaterequest = `UPDATE Productos SET Nombre = ?, Descripcion = ?, Precio = ?, Tipo = ? WHERE id = ?`
-Producto.update = function (nuevoProducto, resultado) {
-    sql.query(updaterequest, values, nuevoProducto, (err, sqlRes) => {
+Producto.update = function (data, resultado) {
+    sql.query(updaterequest, values, data, (err, sqlRes) => {
         if (err) {
             console.log(err);
             resultado(err, null);
         }
-        console.log("Producto creado con exito", { id: sqlRes.insertId, ...nuevoProducto })
-        resultado(null, { id: sqlRes.insertId, ...nuevoProducto });
-    });
+        console.log("Producto creado con exito", sqlRes)
+        resultado(null, sqlRes);
+    })
 }
 module.exports = Producto;
