3f3608ffa70c5d3f3eafeb65435646a11f7ea03f
luisfer <luisfeer20@gmail.com>
2022-05-12 10:52:30 -0600

    Delete

--- controllers/ProductoController.js
+++ controllers/ProductoController.js
@@ -26,7 +26,6 @@ exports.create = (req, res) => {
     })
 };
 exports.selectAll = (req, res) => {
-    console.log('hola putos')
     Producto.selectAll((err, data) => {
         if (err) {
             res.status(500).send({
