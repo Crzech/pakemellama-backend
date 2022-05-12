85e379b15842f24b3b7617cb7fdd96383d87cdfc
luisfer <luisfeer20@gmail.com>
2022-05-12 11:22:59 -0600

    Delete

--- controllers/ProductoController.js
+++ controllers/ProductoController.js
@@ -72,18 +72,15 @@ exports.delete = (req, res) => {
         });
         return;
     }
-    const { id } = req.body
-    const producto = new Producto({
-        id
+    const id = req.body
+    const producto = new Producto(id)
+    Producto.update(producto, (err, data) => {
+        if (err) {
+            res.status(500).send({
+                message: err.message
+            })
+        } else {
+            res.status(201).send({ message: "Producto eliminado", data })
+        }
     })
-    Producto.update(Object.values(producto)
-        , (err, data) => {
-            if (err) {
-                res.status(500).send({
-                    message: err.message
-                })
-            } else {
-                res.status(201).send({ message: "Producto eliminado", data })
-            }
-        })
 }
