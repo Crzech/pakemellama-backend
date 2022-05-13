2b79b405de0379576bcffe7fd78598efe4d89a36
luisfer <luisfeer20@gmail.com>
2022-05-13 11:06:16 -0600

    Delete

--- controllers/ProductoController.js
+++ controllers/ProductoController.js
@@ -66,7 +66,7 @@ exports.update = (req, res) => {
         })
 }
 exports.delete = (req, res) => {
-    const id = req.body.id
+    const id = req.body
     const producto = new Producto(id)
     console.log(Object.values(producto))
     Producto.delete(Object.values(producto), (err, data) => {
